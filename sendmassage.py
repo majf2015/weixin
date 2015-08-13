4b03219b6dd9930e3e57ec7db67cd9bbd4f35780
马金凤 <1021008546@qq.com>
2015-08-13 19:55:38 +0800

    隐藏账号

--- sendmassage.py
+++ sendmassage.py
@@ -1,18 +1,13 @@
 # -*- coding: utf-8 -*-
 from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.support.ui import Select
-from selenium.common.exceptions import NoSuchElementException
-from selenium.common.exceptions import NoAlertPresentException
-import unittest, time, re
+import unittest
 
 class SendMassage(unittest.TestCase):
     def setUp(self):
         self.driver = webdriver.Firefox()
         self.base_url = "https://mp.weixin.qq.com"
-        self.account = '1021008546@qq.com'
-        self.pwd = '587921MJF'
+        self.account = 'your account'
+        self.pwd = 'your password'
     
     def test_login(self):
         driver = self.driver
@@ -22,11 +17,6 @@ class SendMassage(unittest.TestCase):
         driver.find_element_by_id("pwd").clear()
         driver.find_element_by_id("pwd").send_keys(self.pwd)
         driver.find_element_by_id("loginBt").click()
-        driver.find_element_by_link_text(u"群发功能").click()
-        driver.find_element_by_css_selector("span.msg_tab_title").click()
-        driver.find_element_by_css_selector("i.icon36_common.add_gray").click()
-        driver.find_element_by_css_selector("div.appmsg.multi > div.appmsg_mask").click()
-        driver.find_element_by_css_selector("button.js_btn").click()
-        driver.find_element_by_css_selector("button[type=\"button\"]").click()
+
 
     
